bfe8178b44d290430bef5047a8dc50ec5feccb4e
sagajax <129790355+sagajax@users.noreply.github.com>
2024-08-18 21:39:05 +0530

    Add assigned issue to prisma schema

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,31 +7,31 @@ import React, { useEffect, useState } from 'react'
 import Skeleton from 'react-loading-skeleton';
 
 const AssigneeSelect = () => {
-    const {data:users,error,isLoading}=useQuery<User[]>({
-        queryKey:['users'],
-        queryFn: async()=>await axios.get('/api/users').then((res)=>res.data),
-        staleTime:60*1000,
-        retry:3,
+    const { data: users, error, isLoading } = useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: async () => await axios.get('/api/users').then((res) => res.data),
+        staleTime: 60 * 1000,
+        retry: 3,
     });
-    if(isLoading) return <Skeleton />;
-    if(error) return null;
-    
-  return (
-    <Select.Root>
-        <Select.Trigger placeholder='Assign...'/>
-            
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                {users?.map((user)=>(
-                    <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
-                ))}
-                <Select.Item value='1'>Anyone</Select.Item>
-                </Select.Group>
-        </Select.Content>
+    if (isLoading) return <Skeleton />;
+    if (error) return null;
 
-    </Select.Root>
-  )
+    return (
+        <Select.Root>
+            <Select.Trigger placeholder='Assign...' />
+
+            <Select.Content>
+                <Select.Group>
+                    <Select.Label>Suggestions</Select.Label>
+                    {users?.map((user) => (
+                        <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                    ))}
+                    <Select.Item value='1'>Anyone</Select.Item>
+                </Select.Group>
+            </Select.Content>
+
+        </Select.Root>
+    )
 }
 
 export default AssigneeSelect
